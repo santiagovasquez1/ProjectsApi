95adda3bb07517b605ffd74feb26231d8a1096c5
svasquez <svasquez@controldiseno.com>
2021-05-19 21:02:59 -0500

    Agregar archivos de proyecto.

---
+++ ProjectsApi/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ProjectsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController :ControllerBase
+    {
+        [HttpGet("home")]
+        public async Task<IActionResult> home()
+        {
+            return Ok("Hola mundo");
+        }
+        [HttpPost("create")]
+        public async Task<IActionResult> create([FromBody] ProjectModel request)
+        {
+            return Ok(request);
+        }
+
+        [HttpPost("uploadImage/{id}")]
+        public async Task<IActionResult> uploadImage()
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            this.createFolder("uploadsImages");
+
+            try
+            {
+                HttpRequest request = HttpContext.Request;
+                foreach(IFormFile file in request.Form.Files)
+                {
+                    string uploadPath = Path.Combine("uploadsImages", file.FileName);
+                    using ( var fileStream =new FileStream(uploadPath,FileMode.Create))
+                    {
+                       await file.CopyToAsync(fileStream);
+                    }
+                }
+                dict.Add("message", "Imagen guardada correctamente");
+                return Ok(dict);
+            }
+            catch (Exception ex)
+            {
+                dict.Add("message", ex.Message);
+                return BadRequest(dict);
+            }
+        }
+        private void createFolder(string dirPath)
+        {
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+        }
+    }   
+}
---
+++ ProjectsApi/Models/ProjectModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectsApi.Models
+{
+    public class ProjectModel
+    {
+        private string fId;
+
+        public string Id
+        {
+            get { return fId; }
+            set { fId = value; }
+        }
+
+        private string fprojectName;
+
+        public string ProjectName
+        {
+            get { return fprojectName; }
+            set { fprojectName = value; }
+        }
+
+        private int fyear;
+
+        public int Year
+        {
+            get { return fyear; }
+            set { fyear = value; }
+        }
+        private string fimage;
+
+        public string Image
+        {
+            get { return fimage; }
+            set { fimage = value; }
+        }
+
+        private List<string> flanguajes;
+
+        public List<string> Languajes
+        {
+            get { return flanguajes; }
+            set { flanguajes = value; }
+        }
+
+        public ProjectModel()
+        {
+            this.Id = new Guid().ToString();
+        }
+
+    }
+}
